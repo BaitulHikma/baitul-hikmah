efab7fcf58fe0e659d4903d9f7f82fedd29d7bb3
BaitulHikma <tamim.studio.personal@gmail.com>
2026-07-31 22:01:54 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Config file for Baitul Hikmah web app
-// Replace API_URL with your deployed Google Apps Script Web App URL
-const API_URL = "https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbygvcaLsZxazGv9AMFHgLT2-vjDt8H7i8Qf7fsZvtthTylVje3EE1GJKVRdDITjMb6M5A/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbygvcaLsZxazGv9AMFHgLT2-vjDt8H7i8Qf7fsZvtthTylVje3EE1GJKVRdDITjMb6M5A/exec";
+
